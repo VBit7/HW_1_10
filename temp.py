f521e8ef0652b63cc79ff02314084c811c8659ce
Vitalii Bielimov <vbit2000@gmail.com>
2023-11-19 22:29:49 +0200

    temp

---
+++ temp.py
@@ -0,0 +1,86 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        if not value.isalpha():
+            raise ValueError("Invalid name: name should consist of letters only")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone: phone should consist of 10 digits only")
+        super().__init__(value)
+        
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone = Phone(phone)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if not self.find_phone(old_phone):
+            raise ValueError("Phone not found")
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones = '; '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+book = AddressBook()
+
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name.value}: {found_phone}")
+
+book.delete("Jane")
